d18eec4a0a6f93ce404e427998a913b5707983d1
Jesther Costinar <jesther.jc15@gmail.com>
2024-06-09 02:23:48 +0800

    First commit

---
+++ src/main/java/com/batch/employee/EmployeebatchApplication.java
@@ -0,0 +1,13 @@
+package com.batch.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeebatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeebatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch/employee/EmployeebatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeebatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
